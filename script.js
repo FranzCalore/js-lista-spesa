f3cbecf961598452cbf55fb2fbde9396b99b66f3
FranzCalore <franzkaiser1993@gmail.com>
2022-11-15 15:10:01 +0100

    finito il super bonus

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ Quando le stampe in console sono corrette, stampare gli elementi in un elenco pu
 
 let listaSpesa = ["Pane", "Latte", "Cereali", "Cicoria", "Uva Sultanina"];
 let bottonespesa = document.getElementById("bottoneElemento");
-bottonespesa.addEventListener("click", function(){
+let bottoneEliminatore;
+bottonespesa.addEventListener("click", function () {
     let inputspesa = document.getElementById("nuovoElementoSpesa").value;
     listaSpesa.push(inputspesa)
-    document.getElementById("listaSpesa").innerHTML += '<li id="elementospesa'+(listaSpesa.length-1)+'"><p>' + inputspesa + '</p> <button type="button" id="eliminaElemento'+ (listaSpesa.length-1) +'" class="btn btn-outline-secondary">Elimina</button></li>'
-
+    document.getElementById("listaSpesa").innerHTML += '<li id="elementospesa' + (listaSpesa.length - 1) + '"><p>' + inputspesa + '</p> <button type="button" id="elementospesa' + (listaSpesa.length - 1) + '" class="btn btn-outline-secondary">Elimina</button></li>'
+    let bottoneEliminatore = document.getElementById("elementospesa"+(listaSpesa.length-1))
+    bottoneEliminatore.addEventListener("click", function () {
+        document.getElementById("elementospesa"+(listaSpesa.length-1)).classList = "d-none";}
+    )
 })
 
 /* utilizzo un ciclo while
@@ -21,11 +25,15 @@ while (i< listaSpesa.length){
 
 // utilizzo un ciclo for
 
-for (let i = 0; i < listaSpesa.length; i++){
-    console.log (listaSpesa[i])
-    document.getElementById("listaSpesa").innerHTML += '<li id="elementospesa'+i+'"><p>' + listaSpesa[i] + '</p> <button type="button" id="eliminaElemento'+ i +'" class="btn btn-outline-secondary">Elimina</button></li>'
-    document.getElementById("eliminaElemento"+i).addEventListener("click", function(){
-        document.getElementById("elementospesa"+i).classList = "d-none";
-    })
 
+for (let i = 0; i < listaSpesa.length; i++) {
+    console.log(listaSpesa[i])
+    document.getElementById("listaSpesa").innerHTML += '<li id="elementospesa' + i + '"><p>' + listaSpesa[i] + '</p> <button "type="button" id="elementospesa' + i + '" class="btn btn-outline-secondary">Elimina</button></li>'
+}
+
+for (let i=0; i<listaSpesa.length; i++){
+    bottoneEliminatore = document.getElementById("elementospesa"+i)
+    bottoneEliminatore.addEventListener("click", function () {
+        document.getElementById("elementospesa"+i).classList = "d-none";}
+    )
 }
